6ffd38de2395e2f7df06ced5071d631f2af714a0
Henrique Jordão Baier De Azevedo <45922978810@info.local>
2023-03-29 10:44:04 -0300

    algoritimo para calcular media de 5 notas feito

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine($"Digite o nome do aluno");
+string aluno = Console.ReadLine();
+
+
+Console.WriteLine($"Digite as 5 notas do aluno:");
+
+Console.WriteLine($"Nota 1 -");
+int nota1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Nota 2 -");
+int nota2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"Nota 3 -");
+int nota3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Nota 4 -");
+int nota4 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Nota 5 -");
+int nota5 = int.Parse(Console.ReadLine());
+
+float media = ((nota1 + nota2 + nota3 + nota4 + nota5) / 5);
+
+Console.WriteLine($"A media do {aluno} e de {media}");
+
+
+
